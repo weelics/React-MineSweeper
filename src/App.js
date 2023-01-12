e8e9f0b054aeaa1dc0a9965dc2b5d95bc8260922
weelics <smaugeri97@gmail.com>
2023-01-12 15:48:27 +0100

    chore: set new icon for flag

--- src/App.js
+++ src/App.js
@@ -108,21 +108,17 @@ function App() {
           <p className="text-4xl text-red-500 bg-gray-800 w-[33%] h-10 text-center">
             {"23:12"}
           </p>
-          <svg
+          <div
             onClick={() => {
               reset();
             }}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="yellow"
-            className="w-14 h-14 text-center border-1 rounded-full bg-gray-800 border-gray-800 hover:stroke-yellow-300"
+
+            className="relative w-14 h-14 text-center bg-yellow-400 border-2 border-gray-800 items-center rounded-full hover:bg-yellow-300"
           >
-            <path
-              fillRule="evenodd"
-              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z"
-              clipRule="evenodd"
-            />
-          </svg>
+            <span className="absolute bottom-6 left-4 w-2 h-2 bg-gray-800 rounded-full"></span>
+            <span className="absolute bottom-6 right-4 w-2 h-2 bg-gray-800 rounded-full"></span>
+            <span className="absolute top-4 right-[14px] w-6 h-6 border-b-4 border-gray-800 rounded-full"></span>
+          </div>
 
           <p className="text-4xl text-red-500 bg-gray-800 w-[33%] h-10 text-center">
            {points}
--- src/components/block.js
+++ src/components/block.js
@@ -2,10 +2,13 @@ function Block(props) {
   let css ="w-full h-9 md:w-10 md:h-10  border border-gray-900 ";
   if (!props.content.isVisible) css+= "bg-[#C0C0C0] hover:bg-[#d9d9d9]";
   else css += "bg-gray-600";
-  if (props.content.isBomb) css += " bg-red-500";
+  if (props.content.isBomb) css += " bg-red-100";
   let body = "";
   let numberCss = "text-center m-0.5 text-2xl font-extrabold ";
-  if(props.content.isFlagged && props.content.isVisible === false) body = 'f';
+  if(props.content.isFlagged && props.content.isVisible === false) body = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 fill-red-600 ml-1 mt-1">
+  <path fillRule="evenodd" d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z" clipRule="evenodd" />
+</svg>
+;
   else if(props.content.isVisible  && props.content.value !== 0){
     body = props.content.value;
     switch(body){
