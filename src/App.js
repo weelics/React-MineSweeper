6c8f83e27ad24033b14917d6f7f86fc1065b19af
weelics <smaugeri97@gmail.com>
2023-01-13 05:15:26 +0100

    fix: set new flag control

--- src/App.js
+++ src/App.js
@@ -4,102 +4,110 @@ import createTable from "./module/createTable";
 
 const length = 9;
 const nMine = 10;
-let mineInGame = nMine;
 let winnerMine = 0;
 let cssFinishGame = "";
 
-const initBoard = () => createTable(length, nMine);
-
-
 function App() {
-
-  const [board, setBoard] = useState(initBoard);
+  const [board, setBoard] = useState(createTable(length, nMine));
   const [alert, setAlert] = useState("");
   const [show, setShow] = useState(false);
-  const [points, setPoints] = useState(nMine);
-  const [inClicks,setInClicks] = useState(true);
+  const [mine, setMine] = useState(nMine);
+  const [inClicks, setInClicks] = useState(true);
 
   const reset = () => {
-    setShow(false);
-    mineInGame = nMine;
     winnerMine = 0;
-    setPoints(mineInGame);
-    setBoard(initBoard);
-  }
+    setShow(false);
+    setMine(nMine);
+    setBoard(createTable(length, nMine));
+  };
 
-  const setVisible = useCallback((row,col) => {
-    board[row][col].isVisible = true;
-  },[board]);
+  const setVisible = useCallback(
+    (row, col) => {
+      board[row][col].isVisible = true;
+    },
+    [board]
+  );
 
-  function gameOver() {
+  const endGame = () => {
+    setMine(nMine);
+    setShow(true);
+  };
+
+  const gameOver = useCallback(() => {
     setAlert("HAI PERSO!");
-    cssFinishGame = "bg-red-600";
-    setPoints(nMine);
-    setShow(true);
-  }
+    cssFinishGame = "bg-red-600 text-cyan-500";
+    endGame();
+  }, []);
 
-  const gameWin = () => {
+  const gameWin = useCallback(() => {
     setAlert("HAI VINTO!");
-    cssFinishGame = "bg-green-600";
-    setPoints(nMine);
-    setShow(true);
-  }
+    cssFinishGame = "bg-green-600 color-red-500";
+    endGame();
+  }, []);
 
-  const check = useCallback ((row, col) => {
-    if (row < 0 || row >= length || col < 0 || col >= length) return;
-    if (board[row][col].isBomb) return;
-    if (board[row][col].value === 0 && board[row][col].isVisible === false) {
-      board[row][col].isVisible = true;
-      check(row - 1, col - 1);
-      check(row - 1, col);
-      check(row, col - 1);
-      check(row + 1, col + 1);
-      check(row + 1, col);
-      check(row, col + 1);
-      check(row - 1, col + 1);
-      check(row + 1, col - 1);
-    } else if(board[row][col].value !== 0){
-      board[row][col].isVisible = true;
-    }
+  const check = useCallback(
+    (row, col) => {
+      if (row < 0 || row >= length || col < 0 || col >= length) return;
+      if (board[row][col].isBomb) return;
+      if (
+        board[row][col].value === 0 &&
+        board[row][col].isVisible === false &&
+        board[row][col].isFlagged === false
+      ) {
+        board[row][col].isVisible = true;
+        check(row - 1, col - 1);
+        check(row - 1, col);
+        check(row, col - 1);
+        check(row + 1, col + 1);
+        check(row + 1, col);
+        check(row, col + 1);
+        check(row - 1, col + 1);
+        check(row + 1, col - 1);
+      } else if (board[row][col].value !== 0) {
+        board[row][col].isVisible = true;
+      }
+    },
+    [board]
+  );
 
-  },[board]);
+  const checkBoard = useCallback(
+    (key) => {
+      const row = Math.floor(key / length);
+      const col = key % length;
 
-  const checkBoard = useCallback((key) =>{
+      if (!inClicks) {
+        if (mine === 0 && !board[row][col].isFlagged) return;
 
-    const row = Math.floor(key / length);
-    const col = (key % length);
-    if(!inClicks) {
-      if(mineInGame === 0 && !board[row][col].isFlagged) return;
-      board[row][col].isFlagged = !board[row][col].isFlagged;
+        board[row][col].isFlagged = !board[row][col].isFlagged;
+        setBoard([...board]);
+
+        if (board[row][col].isFlagged && !board[row][col].isVisible) {
+          if (board[row][col].isBomb) winnerMine++;
+          setMine((old) => old - 1);
+        } else if (!board[row][col].isFlagged && !board[row][col].isVisible) {
+          if (board[row][col].isBomb) winnerMine--;
+          setMine((old) => old + 1);
+        }
+
+        if (winnerMine === nMine) gameWin();
+        return;
+      }
+
+      if (board[row][col].isFlagged) return;
+
+      if (board[row][col].isBomb) gameOver();
+
+      check(row, col);
+      setVisible(row, col);
       setBoard([...board]);
-      if(board[row][col].isFlagged && !board[row][col].isVisible) {
-        if(board[row][col].isBomb) winnerMine++;
-        mineInGame--;
-      }
-      else if(!board[row][col].isFlagged && !board[row][col].isVisible) {
-        if(board[row][col].isBomb) winnerMine--;
-        mineInGame++;
-      }
-      setPoints(mineInGame);
-      if(winnerMine === nMine) gameWin();
-
-      console.log("mineInGame: " + mineInGame + " winnerMine: " + winnerMine);
-      return;
-    }
-    if(board[row][col].isFlagged) return;
-    if(board[row][col].isBomb) gameOver();
-    check(row,col);
-    setVisible(row,col);
-    setBoard([...board]);
-  },[board, check, inClicks, setVisible]);
+    },
+    [board, check, gameOver, gameWin, inClicks, mine, setVisible]
+  );
 
   return (
-    <div className="mx-auto relative bg-[#C0C0C0] gap-4 md:max-w-min flex flex-col">
-      {show && (
-        <div className={`absolute ${cssFinishGame} opacity-60 top-[135px] w-full h-[357px] md:h-[394px] flex justify-center items-center`}><p className="text-8xl font-extrabold text-cyan-500 text-center">{alert}</p></div>
-      )}
+    <div className="mx-auto bg-[#C0C0C0] gap-4 md:max-w-min flex flex-col">
       <div
-        className="bg-[#9f9f9f]  p-2 border-t-4 border-l-4 
+        className="bg-[#9f9f9f] p-2 border-t-4 border-l-4 
         border-b-4 border-r-4
         border-t-gray-300 border-l-gray-300
         border-b-gray-600 border-r-gray-600"
@@ -112,7 +120,6 @@ function App() {
             onClick={() => {
               reset();
             }}
-
             className="relative w-14 h-14 text-center bg-yellow-400 border-2 border-gray-800 items-center rounded-full hover:bg-yellow-300"
           >
             <span className="absolute bottom-6 left-4 w-2 h-2 bg-gray-800 rounded-full"></span>
@@ -121,7 +128,7 @@ function App() {
           </div>
 
           <p className="text-4xl text-red-500 bg-gray-800 w-[33%] h-10 text-center">
-           {points}
+            {mine}
           </p>
         </div>
       </div>
@@ -132,19 +139,26 @@ function App() {
         border-b-gray-600 border-r-gray-600"
       >
         <div
-          className="flex flex-col md:items-center border-t-4 border-l-4 
+          className="relative flex flex-col md:items-center border-t-4 border-l-4 
         border-b-4 border-r-4
         border-b-gray-300 border-r-gray-300
         border-t-gray-600 border-l-gray-600"
         >
+          {show && (
+            <div
+              className={`absolute ${cssFinishGame} opacity-80 top-0 w-full h-full flex justify-center items-center`}
+            >
+              <p className="text-6xl font-extrabold text-center">{alert}</p>
+            </div>
+          )}
           {board.map((grid) => {
             return (
               <div className="flex">
                 {grid.map((row) => {
                   return (
                     <Block
-                    content={row}
-                    checkBoard={() => checkBoard(row.key)}
+                      content={row}
+                      checkBoard={() => checkBoard(row.key)}
                     />
                   );
                 })}
@@ -160,19 +174,37 @@ function App() {
         border-b-gray-600 border-r-gray-600"
       >
         <div className="flex justify-around items-center ">
-
-          <svg 
-          onClick={() => setInClicks(true)} 
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${inClicks ? 'border-green-600' : 'border-gray-500'} w-20 h-20 border-8 rounded-full  bg-gray-800 fill-red-600 hover:fill-amber-500`}>
-            <path fillRule="evenodd" d="M12 1.5a.75.75 0 01.75.75V4.5a.75.75 0 01-1.5 0V2.25A.75.75 0 0112 1.5zM5.636 4.136a.75.75 0 011.06 0l1.592 1.591a.75.75 0 01-1.061 1.06l-1.591-1.59a.75.75 0 010-1.061zm12.728 0a.75.75 0 010 1.06l-1.591 1.592a.75.75 0 01-1.06-1.061l1.59-1.591a.75.75 0 011.061 0zm-6.816 4.496a.75.75 0 01.82.311l5.228 7.917a.75.75 0 01-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 01-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 01-1.247-.606l.569-9.47a.75.75 0 01.554-.68zM3 10.5a.75.75 0 01.75-.75H6a.75.75 0 010 1.5H3.75A.75.75 0 013 10.5zm14.25 0a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H18a.75.75 0 01-.75-.75zm-8.962 3.712a.75.75 0 010 1.061l-1.591 1.591a.75.75 0 11-1.061-1.06l1.591-1.592a.75.75 0 011.06 0z" clipRule="evenodd" />
+          <svg
+            onClick={() => setInClicks(true)}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className={`${
+              inClicks ? "border-green-600" : "border-gray-500"
+            } w-20 h-20 border-8 rounded-full  bg-gray-800 fill-red-600 hover:fill-amber-500`}
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 1.5a.75.75 0 01.75.75V4.5a.75.75 0 01-1.5 0V2.25A.75.75 0 0112 1.5zM5.636 4.136a.75.75 0 011.06 0l1.592 1.591a.75.75 0 01-1.061 1.06l-1.591-1.59a.75.75 0 010-1.061zm12.728 0a.75.75 0 010 1.06l-1.591 1.592a.75.75 0 01-1.06-1.061l1.59-1.591a.75.75 0 011.061 0zm-6.816 4.496a.75.75 0 01.82.311l5.228 7.917a.75.75 0 01-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 01-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 01-1.247-.606l.569-9.47a.75.75 0 01.554-.68zM3 10.5a.75.75 0 01.75-.75H6a.75.75 0 010 1.5H3.75A.75.75 0 013 10.5zm14.25 0a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H18a.75.75 0 01-.75-.75zm-8.962 3.712a.75.75 0 010 1.061l-1.591 1.591a.75.75 0 11-1.061-1.06l1.591-1.592a.75.75 0 011.06 0z"
+              clipRule="evenodd"
+            />
           </svg>
 
-          <svg 
-          onClick={() => setInClicks(false)} 
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" className={`${inClicks ? 'border-gray-500' : 'border-green-600'} w-20 h-20 border-8 rounded-full bg-gray-800 fill-red-600 hover:fill-amber-500`}>
-            <path fillRule="evenodd" d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z" clipRule="evenodd" />
+          <svg
+            onClick={() => setInClicks(false)}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="red"
+            className={`${
+              inClicks ? "border-gray-500" : "border-green-600"
+            } w-20 h-20 border-8 rounded-full bg-gray-800 fill-red-600 hover:fill-amber-500`}
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z"
+              clipRule="evenodd"
+            />
           </svg>
-
         </div>
       </div>
     </div>
--- src/components/block.js
+++ src/components/block.js
@@ -1,35 +1,55 @@
 function Block(props) {
-  let css ="w-full h-9 md:w-10 md:h-10  border border-gray-900 ";
-  if (!props.content.isVisible) css+= "bg-[#C0C0C0] hover:bg-[#d9d9d9]";
+  let css = "w-full h-[38px] md:w-10 md:h-10 border border-gray-900 ";
+  if (!props.content.isVisible) css += "bg-[#C0C0C0] hover:bg-[#d9d9d9]";
   else css += "bg-gray-600";
-  if (props.content.isBomb) css += " bg-red-100";
+  //if (props.content.isBomb) css += " bg-red-100";
   let body = "";
   let numberCss = "text-center m-0.5 text-2xl font-extrabold ";
-  if(props.content.isFlagged && props.content.isVisible === false) body = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 fill-red-600 ml-1 mt-1">
-  <path fillRule="evenodd" d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z" clipRule="evenodd" />
-</svg>
-;
-  else if(props.content.isVisible  && props.content.value !== 0){
+  if (props.content.isFlagged && props.content.isVisible === false)
+    body = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-6 h-6 fill-red-600 ml-1 mt-1"
+      >
+        <path
+          fillRule="evenodd"
+          d="M3 2.25a.75.75 0 01.75.75v.54l1.838-.46a9.75 9.75 0 016.725.738l.108.054a8.25 8.25 0 005.58.652l3.109-.732a.75.75 0 01.917.81 47.784 47.784 0 00.005 10.337.75.75 0 01-.574.812l-3.114.733a9.75 9.75 0 01-6.594-.77l-.108-.054a8.25 8.25 0 00-5.69-.625l-2.202.55V21a.75.75 0 01-1.5 0V3A.75.75 0 013 2.25z"
+          clipRule="evenodd"
+        />
+      </svg>
+    );
+  else if (props.content.isVisible && props.content.value !== 0) {
     body = props.content.value;
-    switch(body){
-      case 1: numberCss += "text-cyan-500"; break;
-      case 2: numberCss += "text-green-500"; break;
-      case 3: numberCss += "text-red-500"; break;
-      case 4: numberCss += "text-purple-500"; break;
-      default: break;
+    switch (body) {
+      case 1:
+        numberCss += "text-cyan-500";
+        break;
+      case 2:
+        numberCss += "text-green-500";
+        break;
+      case 3:
+        numberCss += "text-red-500";
+        break;
+      case 4:
+        numberCss += "text-purple-500";
+        break;
+      default:
+        break;
     }
-  } 
+  }
 
   return (
     <span
       onClick={() => {
         props.checkBoard();
-      }
-
-      }
+      }}
       className={css}
     >
-      <p className={numberCss}> {props.content.isVisible || props.content.isFlagged ? body : ""}
+      <p className={numberCss}>
+        {" "}
+        {props.content.isVisible || props.content.isFlagged ? body : ""}
       </p>
     </span>
   );
